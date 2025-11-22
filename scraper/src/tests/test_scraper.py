339a2a9237f6ffa9c0e357661ad03b2d32b67bbc
Zussman <zhenya.pyryev@gmail.com>
2025-11-22 18:02:30 +0100

    add test for save_tree and load_tree

---
+++ scraper/src/tests/test_scraper.py
@@ -0,0 +1,22 @@
+import os
+from scraper import Scraper
+
+
+URL = "https://agrochowski.pl/"
+
+
+def test_save_load():
+    OUTPUT = "tests/no_pages_results.json"
+
+    scrap = Scraper(URL)
+
+    scrap.parse_categories(parse_pages=False)
+    
+    tree_before_load = scrap.tree
+
+    scrap.save_tree(OUTPUT)
+    scrap.load_tree(OUTPUT)
+
+    assert tree_before_load == scrap.tree
+
+    os.remove(OUTPUT)
