021e23cc19f796399d256e3640831b1d31c5daea
dyedykyu <dyedykyu@gmail.com>
2026-01-16 13:44:15 +0100

    corrected Dockerfile added Parameters.php

---
+++ config/Parameters.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'parameters' =>
+  array (
+    'database_host' => 'student-swarm01.maas',
+    'database_port' => '',
+    'database_name' => 'BE_201253',
+    'database_user' => 'root',
+    'database_password' => 'student',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'JRgob4oxdKWwTXovEjKMn1G3Bcpz8wBGgUFFHrmm9cjFFBIyEI3EisMRZ8bShAD4',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2025-11-22',
+    'locale' => 'pl-PL',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'q8aLEPbdQSjU5Tp6B0hnLSEijCc4GHbxV4TbtnZYWzUzyVVUHVkI2Uex5YoXgJRt',
+    'cookie_iv' => 'feLm4qNnFxZXE0A3oXLeIT6G1vy8h3IK',
+    'new_cookie_key' => 'def00000ea2f9347764a8934925bfb49b0a556e1b034c2bd11c457ef23bf0368894ca4b142ec7cfd57b4a8665c18cec008aa319e61f2304ca6da803e3e15ea875fe85b27',
+  ),
+);
+
